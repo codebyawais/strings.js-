25556fde870712347fa8c28e4c1fac17f6b60681
WebDevByAwaisRafiq <webdevbyawais@gmail.com>
2024-06-05 14:00:51 +0500

    Add files via upload

---
+++ string.js
@@ -0,0 +1,51 @@
+// string can be written " ", '';
+
+const name="awais";
+const repoaccount = 50;
+
+
+//console.log(name + repoacccount);
+
+console.log(`hello my name is ${name} and my repoaccount is ${repoaccount}`);
+
+
+const gameName= new String ('awais-rafiq')
+console.log(gameName[4]);
+//console.log(gameName__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toLocaleUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('s'));
+
+
+// const newstr="awais rafiq"
+// const newstring= gameName.substring(0, 4)
+// console.log(newstring);
+
+const anotherstr = gameName.slice(-8, 9)
+console.log(anotherstr);
+// slice
+
+
+// trim can be removed the spaces
+const newstringone="   awais    "
+console.log(newstringone);
+console.log(newstringone.trim());
+
+const url ="https:awais.com/awa/chaudhary"
+console.log( url.replace('com', '-'));
+console.log(url.includes('asad'));
+
+
+
+
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
